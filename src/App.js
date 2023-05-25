81b6c2133f6f28d7d6cb94f1f26a3ff41d555012
sujin <atena9604@naver.com>
2023-05-26 01:07:24 +0900

    [feat] todoData -create, delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
 import "./App.css"
 export default class App extends Component {
+    state ={
+        todoData :[
+            {
+                id:"1",
+                title:"공부하기",
+                completed: true
+            },
+            {
+                id:"2",
+                title:"청소하기",
+                completed: false
+            }
+        ],
+        value:""
+    }
     btnStyle = {
         color:"#fff",
         border:"none",
@@ -12,9 +27,29 @@ export default class App extends Component {
     getStyle = () => {
         return {
             padding:"10px",
-            borderBotton: "1px #ccc dotted",
+            borderBottom: "1px #ccc dotted",
             textDecoration: 'none'
         }
+    }
+    handleClick = (id) => {
+        let newTodoData = this.state.todoData.filter(data => data.id !== id);
+        console.log(newTodoData);
+        this.setState({todoData: newTodoData});
+    }
+    handleChange = (e) => {
+        console.log(e.target.value)
+        this.setState({value: e.target.value})
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        let newTodo ={
+            id: Date.now(),
+            title: this.state.value,
+            completed:false,
+        };
+
+        this.setState({todoData: [...this.state.todoData, newTodo]})
     }
     render() {
     return (
@@ -23,11 +58,28 @@ export default class App extends Component {
                 <div className="title">
                     <h1>test</h1>
                 </div>
-                <div style={this.getStyle()}>
-                    <input type={"checkbox"} defaultChecked={false}/>
-                      아이고
-                    <button style={this.btnStyle}>x</button>
-                </div>
+                {this.state.todoData.map(data => (
+                    <div style={this.getStyle()} key={data.id}>
+                        <input type={"checkbox"} defaultChecked={data.completed}/>
+                        {data.title}
+                        <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+                    </div>
+                ))}
+                <form style={{display : "flex"}} onSubmit={this.handleSubmit}>
+                    <input type="text"
+                           name="value"
+                           style={{flex:"10",padding:"5px"}}
+                           placeholder="해야할 일을 입력해 주세요"
+                           value={this.state.value}
+                           onChange={this.handleChange}
+                    />
+                    <input
+                        type="submit"
+                        value="입력"
+                        className="btn"
+                        style={{flex:'1'}}
+                    />
+                </form>
             </div>
         </div>
     )
