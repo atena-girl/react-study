965c9fce1c1563ca7c647c850856c2ab0e13a3a9
sujin <atena9604@naver.com>
2023-11-23 00:23:26 +0900

    [feat] tailwind 설치

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react';
 import "./App.css"
 import List from "./components/List";
+import Form from "./components/Form";
 export default function App() {
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
 
-    const handleChange = (e) => {
-        console.log(e.target.value)
-        setValue(e.target.value)
-    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -28,22 +26,11 @@ export default function App() {
                 <div className="title">
                     <h1>할일 목록 예제</h1>
                 </div>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
                 <List todoData={todoData} setTodoData={setTodoData}/>
-                <form style={{display : "flex"}} onSubmit={handleSubmit}>
-                    <input type="text"
-                           name="value"
-                           style={{flex:"10",padding:"5px"}}
-                           placeholder="해야할 일을 입력해 주세요"
-                           value={value}
-                           onChange={handleChange}
-                    />
-                    <input
-                        type="submit"
-                        value="입력"
-                        className="btn"
-                        style={{flex:'1'}}
-                    />
-                </form>
+                <Form handleSubmit={handleSubmit} value={value} setValue={setValue}></Form>
             </div>
         </div>
     )
